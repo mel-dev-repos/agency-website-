6319ceb834a67244eb65e49eed6c43758093e1d6
Melina <musakhani1379@gmail.com>
2023-10-18 10:11:35 +0330

    bolding and making title og page larger

--- vite-project/src/pages/homePage/components/layout/header.jsx
+++ vite-project/src/pages/homePage/components/layout/header.jsx
@@ -6,8 +6,12 @@ export default function Header() {
     <div className='bg-homePageHeader '>
       <Navbar />
       <div className='flex flex-col items-center'>
-        <h1 className='font-bold text-3xl text-center'>We Build Digital Product <br />
+        <h1 className='font-bold text-[4rem] text-center'>We Build Digital Product <br />
           For Your Success </h1>
+        <div className='flex gap-4 '>
+          <button className='bg-orange text-white p-5 rounded-md'>Get Started</button>
+          <button className='bg-[#F4D3A1] text-orange rounded-md'>Watch Demo</button>
+        </div>
         <img src={HeaderBg} />
       </div>
 
