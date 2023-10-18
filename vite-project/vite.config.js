772b4e044d65540514a010d755759239047dfe45
Melina <musakhani1379@gmail.com>
2023-10-18 10:12:01 +0330

    vite config file created

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      Components: "/src/components",
+      Pages: "/src/pages",
+      Layout: "/src/layout",
+      Images: "/src/assets/images",
+      Data: "/src/assets/data",
+    },
+  },
 })
+
+
